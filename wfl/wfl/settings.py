a874f85e08fab3ff235379c9a681b30e843e8340
Derek Yarnell <derek@umiacs.umd.edu>
2015-11-25 16:00:53 -0500

    Adding a staticfiles root for openshift.

--- wfl/wfl/settings.py
+++ wfl/wfl/settings.py
@@ -103,4 +103,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(os.environ.get('OPENSHIFT_REPO_DIR'),
+                           'wfl', 'static')
