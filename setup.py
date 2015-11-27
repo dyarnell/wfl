2bc61f1265fc522a359b663abf3c0840ffec4f55
Derek Yarnell <derek@umiacs.umd.edu>
2015-11-27 11:06:17 -0500

    Adding the widget teak requirement.

--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ setup(
     url='http://www.python.org/sigs/distutils-sig/',
     # GETTING-STARTED: define required django version:
     install_requires=[
-        'Django==1.8.4'
+        'Django==1.8.4',
+        'django-widget-tweaks==1.3',
     ],
     dependency_links=[
         'https://pypi.python.org/simple/django/'
